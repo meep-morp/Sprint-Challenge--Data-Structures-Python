f18bcbe7b234a0dce699991d59d629ac416bf01d
meep-morp <gracieaerogirl@gmail.com>
2020-08-14 10:45:15 -0500

    Finished Names and reverse

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from BST import BSTNode
 
 start_time = time.time()
 
@@ -10,17 +11,31 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# Return the list of duplicates in this data structure
+duplicates = []
+bst = BSTNode("")
+
+
+def printEach(node):
+    print(f"\n{node}\n")
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+for name in names_1:
+    bst.insert(name)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{bst.for_each(printEach)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
